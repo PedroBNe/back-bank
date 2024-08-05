702cd26569c86e8613dc035b79e6f81c1148e0e3
Pedro Barreto <pedro.barreto.ne@gmail.com>
2024-08-05 19:08:42 -0300

    att:agorafoi

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,14 +2,19 @@ import cors from 'cors';
 
 const corsOptions = {
   origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:3000', 'http://localhost:4200', 'https://mobibank.com.br/', 'http://193.123.96.135:3000',];
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'http://localhost:4200',
+      'https://mobibank.com.br',
+      'http://193.123.96.135:3000'
+    ];    
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   credentials: true, 
 };
 
